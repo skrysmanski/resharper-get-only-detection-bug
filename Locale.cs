0114e84d609ea096645cf49b450f72ced6ae51bd
Sebastian Krysmanski <noreply@manski.net>
2016-12-21 12:46:32 +0100

    Added code to show the problem

---
+++ Locale.cs
@@ -0,0 +1,27 @@
+﻿using JetBrains.Annotations;
+
+namespace ReSharper_GetOnlyDetectionBug
+{
+	public class LanguageInfo
+	{
+		public string LanguageNameInCulture { get; private set; }
+
+		public string Culture { get; private set; }
+
+		public LanguageInfo(string language)
+		{
+			LanguageNameInCulture = language;
+			Culture = language;
+		}
+	}
+
+	public static class Locale
+	{
+		[PublicAPI]
+		public static string GetLanguageFromCulture(string culture)
+		{
+			var lang = new LanguageInfo("English");
+			return lang.Culture;
+		}
+	}
+}
---
+++ SomeClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace ReSharper_GetOnlyDetectionBug
+{
+	public static class SomeClass
+	{
+		[PublicAPI]
+		public static void SomeMethod()
+		{
+			var langs = new LanguageInfo("English");
+			Console.WriteLine(langs.LanguageNameInCulture);
+		}
+	}
+}
